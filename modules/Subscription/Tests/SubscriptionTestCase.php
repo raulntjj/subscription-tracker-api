eaea451a12d8871440ba9f94969e58fab6ea058c
raulntjj <rauloliveira5230@gmail.com>
2026-03-05 10:15:46 -0300

    feat(tests): replace AdaptiveDatabase trait with RefreshDatabase in test cases and update phpunit environment variables

--- modules/Subscription/Tests/SubscriptionTestCase.php
+++ modules/Subscription/Tests/SubscriptionTestCase.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Subscription\Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Modules\Shared\Infrastructure\Tests\Concerns\AdaptiveDatabase;
 
 abstract class SubscriptionTestCase extends BaseTestCase
 {
-    use AdaptiveDatabase;
+    use RefreshDatabase;
 }
